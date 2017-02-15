c48e2bf09907fa1518b2048266d7629adbb2c842
dan2k <sangtong@gmail.com>
2017-02-15 12:28:15 +0700

    ตัด comment ออก

---
+++ t/t2.php
@@ -0,0 +1,7 @@
+<?php 
+	include '../vendor/autoload.php'; 
+	use mPDF; //mPDF ???????? key index ?? autoload_classmap.php 
+	$mpdf = new mPDF(); 
+	$mpdf->WriteHTML(' Hallo World '); 
+	$mpdf->Output(); 
+?>
